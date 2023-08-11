12807fda4aeb94f301d76770d3cad30f07f82bb4
wujf <1985256940@qq.com>
2023-08-12 06:58:17 +0800

    promise.all()手写

--- leetcode/30days/18_promiseAll.js
+++ leetcode/30days/18_promiseAll.js
@@ -1,11 +1,31 @@
-
+/**
+ * 手写promise.all()
+ * @param {Array} funcs 
+ * @returns Array | String
+ */
 const promiseAll = async function (funcs) {
     let result = []
-    
+    let count = 0
+    let len = funcs.length
+    return new Promise((resolve, reject) => {
+        funcs.forEach((p,i) => {
+            p().then(data => {
+                result[i] = data
+                if (++count === len) {
+                    resolve(result)
+                }
+            }).catch(e => {
+                reject(e)
+            })
+        })
+    })
 }
+
 functions = [
     () => new Promise(resolve => setTimeout(() => resolve(5), 200))
 ]
-promiseAll(functions).then(data=>console.log(data)).catch(e => {
+promiseAll(functions).then(data => {
+    console.log(data)
+}).catch(e => {
     console.log(e);
 })
