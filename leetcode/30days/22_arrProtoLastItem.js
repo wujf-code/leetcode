0cfeee04d52d478f6bca0f9111e6da8df1a51838
wujf <1985256940@qq.com>
2023-08-12 08:34:26 +0800

    返回数组最后一个元素

---
+++ leetcode/30days/22_arrProtoLastItem.js
@@ -0,0 +1,16 @@
+/**
+ * 请你编写一段代码实现一个数组方法，
+ * 使任何数组都可以调用 array.last() 方法，
+ * 这个方法将返回数组最后一个元素。
+ * 如果数组中没有元素，则返回 -1 。
+ * 你可以假设数组是 JSON.parse 的输出结果。
+ */
+
+/**
+ * 返回数组最后一个元素或者-1
+ * @returns 
+ */
+
+Array.prototype.last = function () {
+    return this.length ? this.pop():-1
+  }
